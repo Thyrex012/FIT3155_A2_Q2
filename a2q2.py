dd573222686105cf05c92a4c6843bf45f7af205e
Thyrex12 <ekrithyreachlay012@gmail.com>
2026-05-31 15:59:08 +1000

    Finished writting the comments for question 2

--- a2q2.py
+++ a2q2.py
@@ -1,30 +1,40 @@
-import random
-import sys
-
 # Ekrithyreach Lay
 # Student ID: 33698759
 
-"""
-The algorithm computes for a^b mod n in O(logb) time where b is the number of
-bits that we need to loop through.
-"""
+import random
+import sys
+
+#############################
+#   Modular Exponentiation
+#############################
 def modular_exponentiation(a, b, n):
+    """
+    The algorithm computes for a^b mod n in O(logb) time where b is the number of
+    bits that we need to loop through.
+    """
     binary_b = bin(b)
+    # Using the bin command would cause the binary value to have 0x at the front
+    # So I'll do a list slicing to remove it
     binary_b = binary_b[2:]
     result = 1
     base = a % n
     reversed_binary = binary_b[::-1]
+    # As we're looping from left to right the binary needs to be reversed in order
+    # to get the correct base and result
     for bit in reversed_binary:
         if bit == '1':
             result = (result * base) % n
         base = (base * base) % n
     return result
 
-"""
-The overall algorithmic structure was obtained from the psuedocode provided in Week 6's lecture slides.
-We'll assume that n will always be an odd number
-"""
+######################
+#    Miller Rabin
+######################
 def miller_rabin(n, k):
+    """
+    The overall algorithmic structure was obtained from the psuedocode provided in Week 6's lecture slides.
+    We'll assume that n will always be an odd number
+    """
     s = 0
     t = n - 1
     #This loop keeps going until t becomes an odd number
@@ -55,6 +65,9 @@ def miller_rabin(n, k):
             return False
     return True
 
+########################################
+#   Generating a random prime number
+########################################
 def generate_prime_modulus_p(pat):
     m = len(pat)
     t = max(32, m)
@@ -72,6 +85,14 @@ def generate_prime_modulus_p(pat):
             return candidate_number
         
 def compute_r_of(str, start, stop, beta, p):
+    """
+    A Helper function used to compute the r value of a string in the given portion string[stop...start]. 
+    We'll loop from right to left where results from the previous characters will be used to find the result
+    with the current character. 
+    
+    eg say we have string doggo with start=2 and end=0 which corresponds to the string dog. During the for loop
+    we'll get the r of g, then r of og, then r of dog.
+    """
     result = 0
     base = 1
     #say we have abc so its index 0,1,2. start is 2 and stop is 0
@@ -80,13 +101,22 @@ def compute_r_of(str, start, stop, beta, p):
         base = (beta * base) % p
     return result
 
+##############################################
+#   Pattern matching by hashing and checking
+##############################################
 def pattern_match(txt, pat):
+    """
+    The pattern matching algorithm would determine the r value of the pattern first and afterwards we'll determine
+    the r values of each portion within the txt and if a certain portion has the same r value as pat then we'll do
+    explicit comparisons to ensure that they truly match each other.
+    """
     m = len(pat)
     n = len(txt)
     BETA = 128
     result = []
 
-    #Base case as its not possible for the pat to match with the text if it's length exceeds it
+    # its not possible for the pat to match with the text if it's length exceeds it
+    # so we'll return nothing
     if m > n:
         return []
 
@@ -96,10 +126,7 @@ def pattern_match(txt, pat):
     #The r of pat will be used to compare with r portions of txt and if the value are the same we'll need to perform
     #explicit comparisons to see if they match for sure.
     r_of_pat = compute_r_of(pat, m-1, 0,BETA, p_value)
-
-    #Find the first r of txt
     first_r_of_txt = compute_r_of(txt,  m-1, 0, BETA, p_value)
-    curr_r_txt = first_r_of_txt
 
     if first_r_of_txt == r_of_pat:
         for index in range(m):
@@ -107,15 +134,33 @@ def pattern_match(txt, pat):
                 break
         result.append(0) # The hash value of the text is equal to the hash value of the first portion of txt and the characters
                          # match each other as well.
-        
+
+    curr_r_txt = first_r_of_txt    
     beta_m_minus_one = modular_exponentiation(BETA,m-1,p_value)
 
     # We'll need to start from m onwards as it'll be the end point for the txt portion that we'll use to compute for the
     # next r value of txt.
     for i in range(m, n):
-        # We can use the previous r value's of txt to compute for the current r because only the first character in r 
+
+        # Say we have a string abcdefgh and the length of pat is 4, that means we'll need to find r of abcd, bcde, cdef etc...
+        # In the naive approach as we'll be forced to recompute the r values for each each sliding window without taking into
+        # account the previous r_value
+
+        # In the optimised approach below the idea is that say we already computed r for abcd and now we need to compute r
+        # for bcde. We can see that the suffix bcd of abcd, exists within the prefix of bcde. With this we can determine
+        # r of bcde with the optimisation below
+
+        # r_of_abcd = ((a * Beta^3) + (b * Beta^2) + (c * Beta^1) + d) mod p
+        # r_of_bcd_without_a = r_of_abcd - (a * Beta^3)
+
+        # We can easily compute for Beta^3 with our modular exponentiation function which we'll only need to compute
+        # for once
+
+        # r_of_bcde = (r_of_bcd_without_a * Beta) + e) mod p
+
         val_of_first_char_removed = ord(txt[i-m]) * beta_m_minus_one
-        curr_r_txt = ((curr_r_txt - val_of_first_char_removed) * BETA + ord(txt[i])) % p_value
+        r_of_first_char_removed = curr_r_txt - val_of_first_char_removed
+        curr_r_txt = ((r_of_first_char_removed * BETA) + ord(txt[i])) % p_value
         if curr_r_txt == r_of_pat:
             for index in range(m):
                 if txt[i-m+1+index] != pat[index]:
@@ -148,10 +193,4 @@ if __name__ == '__main__':
     with open("output_a2q2.txt", 'w') as f:
         for index in range(len(result)):
             f.write(str(result[index]+1) + '\n')
-
-
-# print(modular_exponentiation(7,560,561))
-# print(generate_prime_modulus_p("abcdefghijklmnop"))
-# print(pattern_match("abcabcabc", "abc"))
-# print(pattern_match("babbaababaababaababab", "aababaabab"))
 
